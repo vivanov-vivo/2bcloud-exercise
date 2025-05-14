9201bc63877f97c7b918a8e3993244c93295f991
Victoria Ivanov <68241162+vivanov-vivo@users.noreply.github.com>
2025-05-14 21:59:29 +0300

    Update providers.tf

--- terraform/ecr/providers.tf
+++ terraform/ecr/providers.tf
@@ -1,18 +1,22 @@
+provider "aws" {
+  region = var.region  
+}
+
 terraform {
   required_providers {
+    helm = {
+      source = "hashicorp/helm"
+      version = ">=2.11.0"  # Use the latest version available
+    }
     aws = {
       source  = "hashicorp/aws"
       version = "~> 3.0"
     }
   }
-}
-
-provider "aws" {
-  region  = "eu-north-1"
-}
- backend "s3" {
+  backend "s3" {
     bucket  = "2bcloud-tf-states"
     key     = "ecr/terraform.tfstate"
     region  = "eu-north-1"
     dynamodb_table = "2bcloud-tf-locks"
     }
+}
