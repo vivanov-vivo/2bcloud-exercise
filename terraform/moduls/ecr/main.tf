86110405eca5104c06ee2dcfea429f1b6849c826
Victoria Ivanov <68241162+vivanov-vivo@users.noreply.github.com>
2025-05-14 13:51:57 +0300

    Create main.tf

---
+++ terraform/moduls/ecr/main.tf
@@ -0,0 +1,11 @@
+resource "aws_ecr_repository" "ecr" {
+  name                 = 2bcloud-ecr
+  image_tag_mutability = var.image_mutability
+  encryption_configuration {
+    encryption_type = var.encrypt_type
+  }
+  image_scanning_configuration {
+    scan_on_push = true
+  }
+  tags = var.tags
+}
