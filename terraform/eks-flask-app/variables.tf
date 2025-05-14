4f1160f33d409e82402cd7204df74e35642a21ab
Victoria Ivanov <68241162+vivanov-vivo@users.noreply.github.com>
2025-05-14 11:47:14 +0300

    Create variables.tf

---
+++ terraform/eks-flask-app/variables.tf
@@ -0,0 +1,12 @@
+variable "region" {
+  default = "eu-north-1"
+}
+
+variable "cluster_name" {
+  default = "flask-eks-cluster"
+}
+
+variable "image_url" {
+  description = "Docker image URL in ECR (e.g. <account>.dkr.ecr.<region>.amazonaws.com/hello-flask-app:latest)"
+  type        = string
+}
