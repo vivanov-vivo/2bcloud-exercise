abc708cc039c422cc0dc29a28f3e61af3ebfa633
Victoria Ivanov <68241162+vivanov-vivo@users.noreply.github.com>
2025-05-15 23:33:27 +0300

    Update main.tf

--- terraform/ecr/main.tf
+++ terraform/ecr/main.tf
@@ -20,3 +20,15 @@ resource "aws_ecr_repository" "ecr1" {
   }
   tags = var.tags
 }
+resource "aws_ecr_repository" "ecr2" {
+  name                 = var.ecr_name[2]
+  image_tag_mutability = var.image_mutability
+  encryption_configuration {
+    encryption_type = var.encrypt_type
+  }
+  image_scanning_configuration {
+    scan_on_push = true
+  }
+  tags = var.tags
+}
+
